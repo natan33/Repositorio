0fb1bacd8ae9aba83286ecaeb78eba8d8076c5f4
natan33 <srassis2014@gmail.com>
2018-11-23 21:35:08 -0200

    segundo commit

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -15,7 +15,8 @@ public class JavaApplication1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("olá mundo");
+        
+        System.out.println("qualquer");
     }
     
 }
